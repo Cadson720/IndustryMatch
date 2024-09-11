ee09b42106b34bff1df9dfb649b19da994142ffe
AaronNSmith <126218145+AaronNSmith@users.noreply.github.com>
2024-09-11 10:28:46 +1000

    Remote Hosted Backend

--- BackEnd/config/database.js
+++ BackEnd/config/database.js
@@ -1,9 +1,11 @@
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-    host: process.env.DB_HOST,
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
     dialect: 'postgres',
+    ssl: {
+        rejectUnauthorized: false // Use if SSL is required, adjust based on your needs
+    }
 });
 
 module.exports = sequelize;
