61535f4c66875015f1d120662b0d6d5d5d886c00
Cadson <ecadden1@outlook.com>
2024-09-23 18:23:42 +1000

    Added savedProject.css and implemented dummy savedProject component for testing

--- FrontEnd/src/components/header.jsx
+++ FrontEnd/src/components/header.jsx
@@ -7,9 +7,9 @@ const Header = () => {
       <div className="inner_header">
         <img src="/efi_logo_simple.png" href="main.jsx" alt="Logo" className="logo" />
         <nav className="nav">
-          <a href="/html pages/projectSearch.html" className="nav-link">Project Search</a>
-          <a href="/html pages/savedProjects.html" className="nav-link">Saved Projects</a>
-          <a href="/html pages/matchMaker.html" className="nav-link">MatchMaker</a>
+          <a href="../projectSearch.html" className="nav-link">Project Search</a>
+          <a href="../savedProjects.html" className="nav-link">Saved Projects</a>
+          <a href="../matchMaker.html" className="nav-link">MatchMaker</a>
         </nav>
       </div>
     </header>
