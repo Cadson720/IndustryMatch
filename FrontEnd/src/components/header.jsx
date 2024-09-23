e5fcf8744bc7cff4e7492038a118f18bf00b3686
Cadson <ecadden1@outlook.com>
2024-09-23 23:21:26 +1000

    going to sleep

--- FrontEnd/src/components/header.jsx
+++ FrontEnd/src/components/header.jsx
@@ -8,7 +8,7 @@ const Header = () => {
         <img src="/efi_logo_simple.png" href="main.jsx" alt="Logo" className="logo" />
         <nav className="nav">
           <a href="../projectSearch.html" className="nav-link">Project Search</a>
-          <a href="../savedProjects.html" className="nav-link">Saved Projects</a>
+          <a href="../savedProject.html" className="nav-link">Saved Projects</a>
           <a href="../matchMaker.html" className="nav-link">MatchMaker</a>
         </nav>
       </div>
