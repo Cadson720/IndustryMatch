47237f986103cd77ddbac52abe565e295932eae1
Br3adly <braedon.cd.james@gmail.com>
2024-10-14 17:03:47 +1100

    Footer

--- FrontEnd/src/pages/landing.jsx
+++ FrontEnd/src/pages/landing.jsx
@@ -5,11 +5,13 @@ import React from 'react';
 // import Landing from '../components/landing.jsx'
 import Landing from '../components/landing.jsx'
 import Header from '../components/header.jsx'
+import Footer from '../components/footer.jsx'
 
 
 createRoot(document.getElementById('root')).render(
     <BrowserRouter>
       <Header />
       <Landing/>
+      <Footer />
     </BrowserRouter>,
   )
